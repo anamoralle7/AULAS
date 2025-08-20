b056e7c8bde92a0a6ad56092f60ad50d7d400df6
anamoralle7 <anajuliamerlo1@gmail.com>
2025-08-20 15:59:51 -0300

    aulaaaaaa

---
+++ 02-aula/script.js
@@ -0,0 +1,96 @@
+//interacao com o usuario via prompt
+/* var nome = prompt("digite um nome:")
+
+alert("seu nome é:" + nome)
+
+// CONDICIONAIS
+
+// SE, SENÃO SE, SENÃO
+// IF, IF ELSE, ELSE
+
+var estaVivo = true 
+
+if(estaVivo == true){
+    console.log("parabens você esta vivo")
+}
+else if (estaVivo == false){
+    console.log("infelizmente, estás mortoo !!!")
+}
+else{
+    console.log("você não está nada, virou sei lá")
+}
+
+// ESCOLHA / CASO
+// SWITCH / CASE
+
+var corCamisa = "Verde"
+
+switch(corCamisa){
+    case "Azul":
+        console.log("vc ganhou um voucher")
+    break
+     case "rosa":
+        console.log("vc ganhou um chaveiro")
+    break
+     case "branca":
+        console.log("vc ganhou um brinde")
+    break
+    default:
+        console.log("cor de camisa invalida")
+        break
+}
+/*
+
+//ATIVIDADE NOTAAA
+/*
+var nota = prompt("digite a sua nota:")
+
+if(nota > 6){
+        console.log("você está aprovado eba eba ebaaaaaaaaa")
+}
+else if(nota >= 5  && nota <= 6){
+        console.log("você está de recuperação sonsoooo")
+       
+}
+else{
+        console.log("você está reprovado burroooooooo")
+      
+}
+
+*/
+
+
+
+// atividade doiessss
+
+
+var idade = prompt("digite a sua idade:")
+var idade2 = prompt("digite a sua idade:")
+var ingresso1, ingresso2
+
+if(idade <= 17){
+    ingresso1 = 15
+}
+else if(idade  >= 18 && idade1 < 59){
+    ingresso1 = 30
+}
+else{
+        ingresso1 = 20 
+}
+
+
+if(idade2 <= 17){
+    ingresso2 = 15
+}
+else if(idade2  >= 18 && idade2 < 59){
+    ingresso2 = 30
+}
+else{
+        ingresso2 = 20 
+}
+var valorFinal = ingresso1 + ingresso2
+
+console.log("O preço final foi:" + valorFinal)
+
+
+
